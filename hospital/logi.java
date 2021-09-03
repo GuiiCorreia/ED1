d8c58ef3140cc2e1d88aa5b78ec524489d245877
GuiiCorreia29581 <78569072+GuiiCorreia29581@users.noreply.github.com>
2021-09-03 10:22:22 -0300

    Add files via upload

---
+++ hospital/logi.java
@@ -0,0 +1,22 @@
+package hospital;
+
+public class logi {
+    int id;
+    String sexo;
+    int idade;
+    boolean gestante;
+    boolean lactante;
+    boolean necessidadeEspecial;
+    int prioridade =  0;
+    logi proximo;
+    public logi(){}
+    public logi(int id, String sexo, int idade, boolean gestante, boolean lactante, boolean necessidadeEspecial, int prioridade){
+        this.id = id;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.gestante = gestante;
+        this.lactante = lactante;
+        this.necessidadeEspecial = necessidadeEspecial;
+        this.prioridade = prioridade;
+    }
+}
